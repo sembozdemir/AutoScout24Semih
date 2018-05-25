9993ea4cfb2a3edb9933b3bc7b9b1677eee36711
Semih Bozdemir <semih.bozdemir@commencis.com>
2018-05-26 01:40:44 +0300

    Edit getVehicles methods

--- app/src/main/java/com/sembozdemir/autoscout24/network/VehicleService.kt
+++ app/src/main/java/com/sembozdemir/autoscout24/network/VehicleService.kt
@@ -3,10 +3,9 @@ package com.sembozdemir.autoscout24.network
 import com.sembozdemir.autoscout24.network.model.Vehicle
 import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.Url
 
 interface VehicleService {
 
-    @GET
-    fun getVehicles(@Url dummyEndpoint: String = ""): Single<List<Vehicle>>
+    @GET("/")
+    fun getVehicles(): Single<List<Vehicle>>
 }
